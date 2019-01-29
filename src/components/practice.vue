c07d34896ad9017e00451d7fd9cad56c8d5ebbb0
juli_e <julia.ero@euedge.com>
2019-01-29 13:58:52 +0100

    ui + add / remove intervals

--- src/components/practice.vue
+++ src/components/practice.vue
@@ -1,203 +1,263 @@
 <template>
   <div>
-    <h1>Repe-tone</h1>
-    <h2>Hear the sound and tell the the space</h2>
+    <h1>RepeTone</h1>
+    <h2>Hear the sound and tell the space</h2>
     <div class="playground">
       <button @click="play" class="play">play</button>
       <div class="row">
         <div class="input-group" v-for="interval in intervalAnswers">
-          <input type="radio" name="answer" :id="interval.name" :value="interval.name" v-model="answer" @change="checkInterval">
-          <label :for="interval.name">{{interval.label}}</label>
+          <input type="radio" name="answer" :id="interval.name + 'answer'" :value="interval.name" v-model="answer"
+                 @change="checkInterval">
+          <label :for="interval.name + 'answer'">{{interval.label}}</label>
         </div>
       </div>
-      <button @click="getNext" v-if="result" class="next">play next</button>
+      <button @click="getNext" class="next" :class="result && 'show'">play next</button>
     </div>
     <div class="settings">
-          <div class="settings-column">
-            <h2>direction</h2>
-            <div class="column">
-              <div class="input-group" v-for="dir in directionOptions">
-                <input type="radio" name="direction" :id="dir.id" :value="dir.id" v-model="direction">
-                <label :for="dir.id">{{dir.label}}</label>
-              </div>
-            </div>
-          </div>
-          <div class="settings-column">
-            <h2>select intervals</h2>
-            <div class="column">
-              <div v-for="item in intervals" >
-                <input type="checkbox" :id="item.name" :value="item.name" v-model="selectedIntervals">
-                <label :for="item.name">{{item.label}}</label>
-              </div>
-            </div>
+      <div class="settings-column">
+        <h2>direction</h2>
+        <div class="column">
+          <div class="input-group" v-for="dir in directionOptions">
+            <input type="radio" name="direction" :id="dir.id" :value="dir.id" v-model="direction">
+            <label :for="dir.id">{{dir.label}}</label>
           </div>
         </div>
+      </div>
+      <div class="settings-column">
+        <h2>select intervals</h2>
+        <div class="column">
+          <div v-for="item in intervals" class="input-group">
+            <input type="checkbox" :id="item.name+ 'int'" :value="item.name" :checked="intervalIsSelected(item.name)" @change="selectIntervals">
+            <label :for="item.name + 'int'">{{item.label}}</label>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import { samplesByInstrument } from '../utils/instruments'
-import { intervals, addInterval, getRandomNote } from '../utils/intervals'
+  import { samplesByInstrument } from '../utils/instruments'
+  import { intervals, addInterval, getRandomNote } from '../utils/intervals'
 
-/* global Tone */
-/* eslint-disable no-console */
+  /* global Tone */
+  /* eslint-disable no-console */
 
-export default {
-  name: 'practice-scene',
-  data () {
-    return {
-      sampler: null,
-      startNote: '',
-      intervals,
-      selectedIntervals: ['M3', 'P5'],
-      directionOptions: [
-        {id: 'asc', label: 'ascending'},
-        {id: 'desc', label: 'descending'},
-        {id: 'random', label: 'any direction'}
-      ],
-      direction: 'asc',
-      answer: '',
-      result: false,
-      interval: ''
-    }
-  },
-  computed: {
-    intervalAnswers() {
-      return intervals.filter(item => this.selectedIntervals.indexOf(item.name) >= 0)
+  export default {
+    name: 'practice-scene',
+    data () {
+      return {
+        sampler: null,
+        startNote: '',
+        intervals,
+        selectedIntervals: ['M3', 'P5'],
+        directionOptions: [
+          {id: 'asc', label: 'ascending'},
+          {id: 'desc', label: 'descending'},
+          {id: 'random', label: 'any direction'},
+        ],
+        direction: 'asc',
+        answer: '',
+        result: false,
+        interval: '',
+        stats: []
+      }
     },
-    randomDirection() {
-      const dirs = ['asc', 'desc']
-      const i = Math.floor(Math.random() * dirs.length)
-      return dirs[i]
+    computed: {
+      intervalAnswers () {
+        return intervals.filter(item => this.selectedIntervals.indexOf(item.name) >= 0)
+      },
+      randomDirection () {
+        const dirs = ['asc', 'desc']
+        const i = Math.floor(Math.random() * dirs.length)
+        return dirs[i]
+      },
+      selectedDirection () {
+        return this.direction === 'random' ? this.randomDirection() : this.direction
+      },
+      intervalIsSelected () {
+        return (val) => this.selectedIntervals.indexOf(val) >= 0
+      }
     },
-    selectedDirection() {
-      return this.direction === 'random' ? this.randomDirection() : this.direction
-    }
-  },
-  methods: {
-    play() {
-      const noteB = addInterval(this.startNote, this.interval, this.selectedDirection)
+    methods: {
+      play () {
+        const noteB = addInterval(this.startNote, this.interval, this.selectedDirection)
 
-      this.sampler.triggerAttackRelease(this.startNote, '4n', 0, 0.7)
-      this.sampler.triggerAttackRelease(noteB, '4n', '4n', 0.7)
+        this.sampler.triggerAttackRelease(this.startNote, '4n', 0, 0.7)
+        this.sampler.triggerAttackRelease(noteB, '4n', '4n', 0.7)
+      },
+      checkInterval () {
+        this.result = this.answer === this.interval
+        this.saveStats()
+      },
+      getRandomInterval () {
+        const random = Math.floor(Math.random() * this.selectedIntervals.length)
+        return this.selectedIntervals[random]
+      },
+      getNext () {
+        this.startNote = getRandomNote('piano')
+        this.interval = this.getRandomInterval()
+        this.result = false
+        this.answer = ''
+      },
+      saveStats () {
+        this.stats.forEach(item => {
+          if (item.name === this.interval) {
+            item.total++
+            item.found = this.result && item.found + 1
+            item.accuracy = item.found / item.total
+          }
+        })
+      },
+      selectIntervals(event) {
+        const currentInterval = event.target.value
+
+        if (this.selectedIntervals.indexOf(currentInterval) > 0) {
+          this.selectedIntervals = this.selectedIntervals.filter(i => i !== currentInterval)
+        } else {
+          this.selectedIntervals.push(currentInterval)
+        }
+        console.log(currentInterval, this.selectedIntervals)
+      }
     },
-    checkInterval() {
-      this.result = this.answer === this.interval
-    },
-    getRandomInterval() {
-      const random = Math.floor(Math.random() * this.selectedIntervals.length)
-      return this.selectedIntervals[random]
-    },
-    getNext() {
+    mounted () {
+      let piano = samplesByInstrument('piano')
+      this.sampler = new Tone.Sampler(piano.notes, {baseUrl: piano.baseUrl, release: 1})
+
+      Tone.Buffer.on('load', () => {
+        this.sampler.toMaster()
+      })
+
       this.startNote = getRandomNote('piano')
       this.interval = this.getRandomInterval()
-      this.result = false
-      this.answer = ''
+      this.stats = this.selectedIntervals.map(i => {
+        return {
+          name: i,
+          total: 0,
+          found: 0,
+          accuracy: 0
+        }
+      })
     }
-  },
-  mounted() {
-    let piano = samplesByInstrument('piano')
-    this.sampler = new Tone.Sampler(piano.notes, {baseUrl: piano.baseUrl, release: 1})
-
-    Tone.Buffer.on('load', () => {
-      this.sampler.toMaster()
-    })
-
-    this.startNote = getRandomNote('piano')
-    this.interval = this.getRandomInterval()
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  h3 {
+    margin: 40px 0 0;
+  }
 
-.row {
-  display: flex;
-  justify-content: center;
-}
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
 
-.column {
-  display: flex;
-  width: 100%;
-  height: 80%;
-  flex-direction: column;
-  flex-wrap: wrap;
-  align-items: center;
-}
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
 
-.playground {
-  margin-bottom: 100px;
-}
+  a {
+    color: #42b983;
+  }
 
-.settings {
-  display: flex;
-  width: 960px;
-  height: 260px;
-  margin: 0 auto;
-  justify-content: center;
-}
+  .row {
+    display: flex;
+    justify-content: center;
+    margin: 15px 0;
+  }
 
-.settings-column:first-child  {
-  width: 20%;
-}
+  .column {
+    display: flex;
+    width: 100%;
+    height: 60%;
+    flex-direction: column;
+    flex-wrap: wrap;
+    align-items: center;
+  }
 
-.settings-column:last-child  {
-  width: 80%;
-}
+  .playground {
+    margin-bottom: 100px;
+  }
 
-button {
-  width: 120px;
-  padding: 7px 12px;
-  margin: 0 5px;
-  font-size: 18px;
-  color: #fff;
-  border-radius: 5px;
-  border: 0;
-}
+  .settings {
+    display: flex;
+    width: 960px;
+    height: 260px;
+    margin: 0 auto;
+    justify-content: center;
+  }
 
-.play {
-  background: #2ec4a7;
-}
+  .settings-column:first-child {
+    width: 20%;
+  }
 
-.next {
-  background: #f7de30;
-}
+  .settings-column:last-child {
+    width: 80%;
+  }
 
-input {
-  visibility: hidden
-}
+  .settings-column h2 {
+    text-align: left;
+    padding-left: 30px;
+  }
 
-label {
-  display: block;
-  width: 120px;
-  padding: 4px 6px;
-  margin: 0 5px;
-  font-size: 16px;
-  color: #4a4754;
-  border-radius: 5px;
-  border: 1px solid #4a4754;
-  margin-right: 10px;
-  margin-bottom: 5px;
-}
+  button {
+    width: 120px;
+    padding: 7px 12px;
+    font-size: 17px;
+    font-weight: 600;
+    letter-spacing: 0.02em;
+    color: #fff;
+    border-radius: 5px;
+    border: 0;
+  }
 
-input:checked + label {
-  color: #26a28a;
-  border: 2px solid #26a28a;
-  font-weight: 700;
-}
+  .play {
+    background: #39f9c6;
+  }
+
+  .next {
+    opacity: 0;
+    background: #a373f8;
+  }
+
+  .show {
+    opacity: 1;
+  }
+
+  input {
+    position: absolute;
+    opacity: 0.01;
+  }
+
+  label {
+    position: absolute;
+    display: block;
+    width: 100%;
+    padding: 4px 6px;
+    font-size: 16px;
+    color: #4a4754;
+    border-radius: 5px;
+    border: 1px solid #4a4754;
+    cursor: pointer;
+    box-sizing: border-box;
+  }
+
+  label:hover {
+    background: rgba(52, 235, 172, 0.1);
+  }
+
+  input:checked + label {
+    color: #39f9c6;
+    border: 2px solid #12f8be;
+    font-weight: 700;
+  }
+
+  .input-group {
+    position: relative;
+    width: 120px;
+    height: 36px;
+    margin: 0 5px 5px;
+  }
 </style>
