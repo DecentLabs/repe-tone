9cefeb6dc9e444a7e525ed04a787c6deb7fbdef3
juli_e <julia.ero@euedge.com>
2019-01-31 15:57:39 +0100

    new build

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,5 @@
 module.exports = {
-  chainWebpack: config => {
-    config.module
-    .rule('url')
-    .test(/\.mp3|\.ogg$/)
-    .use('url-loader')
-    .loader('url-loader')
-    .options({
-      limit: 10000,
-      name: 'samples/[name].[ext]'
-    })
-  }
+  lintOnSave: true,
+  baseUrl: '',
+  outputDir: 'docs'
 }
