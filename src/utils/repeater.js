61954bca862bded363b1e4b7c8c046f159ee28f2
juli_e <julia.ero@euedge.com>
2019-02-01 15:43:47 +0100

    algo wip

--- src/utils/repeater.js
+++ src/utils/repeater.js
@@ -11,6 +11,7 @@ function checkProgress(stats) {
 }
 
 function repeatAfterProgress(intervals) {
+  const initArray = ['M3', 'P5', 'P8', 'M2', 'm2', 'm3', 'P4', 'M6', 'tri', 'M7', 'm6', 'm7']
   const progs = intervals.reduce((acc, curr) => {
     let name = curr.name
     let prog = this.getIntervalProgress(name)
@@ -24,17 +25,15 @@ function repeatAfterProgress(intervals) {
     return acc
   }, {bad: [], good: [], unknown: []})
 
-  if (!(progs.bad.length >= 2 && progs.good.length >= 3)) {
-    return null
-  } else {
+  if (progs.bad.length >= 2 && progs.good.length > progs.bad.length + 1) {
     const bads = progs.bad.length > 2 ? progs.bad.sort(() => 0.5 - Math.random()).slice(0, 2) : progs.bad
     const goods = progs.good.length > 3 ? progs.bad.sort(() => 0.5 - Math.random()).slice(0, 3) : progs.goods
 
     const bucket = Array.from({ length: 3 }, () => bads).flat().concat(goods)
     return bucket
+  } else if ((progs.bad.length >= 2 && progs.good.length <= progs.bad.length + 1)) {
+
   }
-
-
 }
 
 export {
